45bb67435de3558cddb01180155cc9fef1a945f1
Jonathan Hollenbeck <jonoepd@gmail.com>
2017-12-03 21:33:15 -0800

    add ipynb script for year 7

--- sql/war_diag.sql
+++ sql/war_diag.sql
@@ -1,7 +1,7 @@
 
 DROP TABLE IF EXISTS bw_diag;
 
-SELECT R.player_id, COUNT(DISTINCT R.year), MAX(W.position) AS pos
+SELECT R.player_id, COUNT(DISTINCT R.year)
 INTO bw_diag
 FROM batting_sy R, batting_war W
 WHERE R.player_id = W.player_id AND R.year = W.year
@@ -12,7 +12,7 @@ DROP TABLE IF EXISTS bw_diff;
 SELECT A.*, total_years - war_years AS diff
 INTO bw_diff
 FROM (
-SELECT A.player_id, A.count AS total_years, COALESCE(D.count,0) AS war_years, COALESCE(D.pos,'Unknown') AS pos
+SELECT A.player_id, A.count AS total_years, COALESCE(D.count,0) AS war_years
 FROM (
 SELECT player_id, COUNT(*)
 FROM batting_sy
